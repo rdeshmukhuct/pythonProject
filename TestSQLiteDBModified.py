fc48127785beeb876b781c6c6cb7a2fabd1ab34a
Gurseerat Singh <gurseer.singh@uct.com>
2021-10-05 14:32:59 -0700

    Added the Summary feature

---
+++ TestSQLiteDBModified.py
@@ -0,0 +1,47 @@
+import sqlite3 as lite
+import sys
+import SQLiteDBModified
+
+ob = SQLiteDBModified.SQLDbModified
+
+print("############### January 2021 ##################### ")
+ob.get_january_description()
+#ob.get_january_sentiments()
+
+print("############### February 2021 ##################### ")
+ob.get_feb_description()
+#ob.get_feb_sentiments()
+
+print("############### March 2021 ##################### ")
+ob.get_march_description()
+#ob.get_march_sentiments()
+
+print("############### April 2021 ##################### ")
+ob.get_april_description()
+#ob.get_april_sentiments()
+
+print("############### May 2021 ##################### ")
+ob.get_may_description()
+#ob.get_may_sentiments()
+
+print("############### June 2021 ##################### ")
+ob.get_june_description()
+#ob.get_june_sentiments()
+
+print("############### July 2021 ##################### ")
+ob.get_july_description()
+#ob.get_july_sentiments()
+
+print("############### August 2021 ##################### ")
+ob.get_september_description()
+#ob.get_september_sentiments()
+
+print("############### September 2021 ##################### ")
+ob.get_august_description()
+#ob.get_august_sentiments()
+
+
+
+
+
+#ob.close()
